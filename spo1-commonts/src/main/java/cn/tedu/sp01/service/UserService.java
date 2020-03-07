bad9e8c35453436e16f1f5510a0a9e1d09d9f1b6
Administrator <Administrator@192.168.64.1>
2020-03-07 12:04:28 +0800

    112

---
+++ spo1-commonts/src/main/java/cn/tedu/sp01/service/UserService.java
@@ -0,0 +1,10 @@
+package cn.tedu.sp01.service;
+
+import cn.tedu.sp01.pojo.User;
+
+public interface UserService {
+//根据用户ID，获取用户数据
+	User getUser(Integer id);
+	//增加用户积分
+	 void addScore(Integer id, Integer score);
+}
